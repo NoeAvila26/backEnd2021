be286ede3e308ce9b34e167f9c3e7b91775750e2
NoeAvila26 <noerr_@hotmail.com>
2021-05-13 18:17:08 -0500

    add/end-points/fileSystem

--- second-express/index.js
+++ second-express/index.js
@@ -71,6 +71,7 @@
 //crear un proyecto -> regresar un json con una lista de koders
 //crear un archivo json 
 //es un objeto con una key koders que dentro tiene un arreglo
+const { request } = require('express')
 const express = require('express')
 const server = express()
 server.use(express.json())
@@ -80,7 +81,7 @@ const fs = require('fs')
 server.get('/koders', (request, response) => {
     const kodersData = fs.readFileSync('koders.json', 'utf8')
     const jsonData = JSON.parse(kodersData)
-
+    //response.json(jsonParsed.koders) version resumida de la parte de abajo
     response.json({
       success:true,
       message: 'koders API',
@@ -88,7 +89,111 @@ server.get('/koders', (request, response) => {
     })
   })
   
-  server.listen(8080, () => {
-    console.log('server listening in port 8080')
+  // server.get('/koders', async (request, response) => {
+  // const json = await fs.promises.readFile('koders.json', 'utf8')
+  // const jsonData = JSON.parse(kodersData)
+  //   response.json(jsonParsed.koders)
+  // })
+
+
+  server.post('/koders', (request, response) => {
+    const name = request.body.name
+    const gender = request.body.gender
+
+    const newKoder = { name, gender }
+
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content)
+
+    json.koders.push(newKoder)
+
+    fs.writeFileSync('koders.json', JSON.stringify(json, null, 2), 'utf8')
+    
+    response.json({
+      success: true
+    })
   })
+
+  server.patch('/koders/:id', (request, response) => {
+    const id =  parseInt(request.params.id)
+    const name = request.body.name
+
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content)
+
+    const newKoders = json.koders.reduce((koders, koderActual) => {
+      if(id === koderActual.id){
+        koderActual.name = name
+      }
+
+      return [
+        ...koders,
+        koderActual
+      ]
+    }, [])
+
+    json.koders = newKoders
+
+    fs.writeFileSync('koders.json', JSON.stringify(json, null, 2), 'utf8')
+    
+    response.json({
+      success: true
+    })
+  })
+
+
+  server.delete('/koders/:id', (request, response) => {
+    const id = request.params.id
+    
+
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content)
+
+    const newKoders = json.koders.filter(koder => koder.id != id) 
+
+    json.koders = newKoders
+
+    fs.writeFileSync('koders.json', JSON.stringify(json, null, 2), 'utf8')
+
+    response.json({
+      success: true
+    })
+    
+  })
+
+
   
+
+  //practica
+  //Delete/koders/:id
+  //Get/koders/id
+
+ server.get('/koders/:id', (request, response) => {
+   const id = request.params.id
+
+   const content = fs.readFileSync('koders.json', 'utf8')
+   const json = JSON.parse(content)
+
+   const koderFound = json.koders.find((koder) => koder.id == id)
+
+
+   if(!koderFound) {
+     response.status(404)
+     responsejson({
+       success: false,
+       message: 'koder not found :c'
+
+     })
+   }
+   response.json({
+     success: true,
+     message: 'koder found',
+     data: {
+       koder: koderFound
+     }
+   })
+ })
+
+ server.listen(8080, () => {
+  console.log('server listening in port 8080')
+})
